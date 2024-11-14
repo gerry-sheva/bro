69456d73c33ba00a213da874b0a0c49a15a9e821
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-11-15 02:15:36 +0700

    feat: implement basic chat feature

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
 use axum::routing::get;
-use socketioxide::extract::SocketRef;
-use socketioxide::SocketIo;
+use serde::{Deserialize, Serialize};
+use socketioxide::{
+    extract::{Data, SocketRef},
+    SocketIo
+};
 use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
+#[derive(Debug, Deserialize)]
+struct MessageIn {
+    room: String,
+    text: String,
+}
+
+#[derive(Serialize)]
+struct MessageOut {
+    text: String,
+    user: String,
+    time: chrono::DateTime<chrono::Utc>,
+}
+
 async fn on_connect(socket: SocketRef) {
     info!("Connecting to socket {:?}", socket);
+
+    socket.on("join", |socket: SocketRef, Data::<String>(room)| {
+        info!("Connected to {:?}", room);
+        let _ = socket.leave_all();
+        let _ = socket.join(room);
+    });
+
+    socket.on("message", |socket: SocketRef, Data::<MessageIn>(data)| {
+        info!("Received message {:?}", data);
+
+        let response = MessageOut {
+            text: format!("{} bro", data.text),
+            user: format!("user-{}", socket.id),
+            time: chrono::Utc::now(),
+        };
+
+        let _ = socket.within(data.room).emit("message", &response);
+    })
 }
 
 #[tokio::main]
