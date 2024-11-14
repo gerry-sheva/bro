3a92f6be271f9791df86f1431d4338e43df7c9f7
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-11-15 01:54:08 +0700

    fix: fix cors permission

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::routing::get;
 use socketioxide::extract::SocketRef;
 use socketioxide::SocketIo;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
@@ -18,7 +20,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let app = axum::Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .layer(layer);
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+                .layer(layer)
+        );
 
     info!("So, so disappointing");
 
